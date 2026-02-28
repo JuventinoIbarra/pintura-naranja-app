f45146ea676e2d25801e9b47d4b7f44154f00b1b
Juventino Ibarra <al07133783@tecmilenio.mx>
2026-02-27 19:56:07 -0600

    Preparación para Produccion

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,10 @@ const mongoose =require("mongoose");
 const connectDB = async () => {
     try{
         //Para pruebas en local
-        await mongoose.connect(process.env.DB_URI)
+        //await mongoose.connect(process.env.DB_URI)
 
         //Para Produccion
-        //await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB conectado");
         }catch(error){
             console.error(error);
